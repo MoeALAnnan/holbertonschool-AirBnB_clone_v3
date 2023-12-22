484c7da1cac71ae701113dea46871a0ecb35a419
MoeAlAnnan <5653@holbertonstudents.com>
2023-12-22 09:51:47 +0000

    starting from 0

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -15,7 +15,7 @@ def user_page(user_id=None):
     if request.method == "GET":
         if user_id is None:
             ret = [obj.to_dict() for obj in storage.all(User).values()]
-            return jsonify(ret), 200
+            
         else:
             obj = storage.get(User, user_id)
             if obj is None:
@@ -32,10 +32,7 @@ def user_page(user_id=None):
         if not request.is_json:
             return 'Not a JSON', 400
         req_dict = request.get_json()
-        if 'email' not in req_dict:
-            return 'Missing email', 400
-        if 'password' not in req_dict:
-            return 'Missing password', 400
+        
 
         new_user = User(**req_dict)
         new_user.user_id = user_id
@@ -43,8 +40,7 @@ def user_page(user_id=None):
         return jsonify(new_user.to_dict()), 201
 
     elif request.method == "PUT":
-        if user_id is None:
-            abort(404)
+        
         obj = storage.get(User, user_id)
         if obj is None:
             abort(404)
