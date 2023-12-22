8d7c4719dd9aed65d6b00989132babeb4eb9c082
MoeAlAnnan <5653@holbertonstudents.com>
2023-12-22 09:52:59 +0000

    fixing if statements

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -32,7 +32,10 @@ def user_page(user_id=None):
         if not request.is_json:
             return 'Not a JSON', 400
         req_dict = request.get_json()
-        
+        if 'email' not in req_dict:
+            return 'Missing email', 400
+        if 'password' not in req_dict:
+            return 'Missing password', 400
 
         new_user = User(**req_dict)
         new_user.user_id = user_id
