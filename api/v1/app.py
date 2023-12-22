fa188ece3ce9cecf43be0070dd82be0cc66e3cc7
MoeAlAnnan <5653@holbertonstudents.com>
2023-12-22 02:24:23 +0000

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,18 +5,25 @@ from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 import os
+from flask_cors import CORS
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+instanceCORS = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
 @app.teardown_appcontext
 def app_teardown(err):
     storage.close()
+
+
 @app.errorhandler(404)
 @app.errorhandler(405)
 def _handle_api_error(ex):
-    return jsonify({"error": "Not found"}), ex.code
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
     host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT", 5050))
+    port = int(os.environ.get("HBNB_API_PORT", 5000))
     app.run(host=host, port=port, threaded=True)
