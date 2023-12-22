4e7cfe3fc3f08a341723d86a5a53e15dbba52e73
MoeAlAnnan <5653@holbertonstudents.com>
2023-12-22 10:50:42 +0000

    adding put method

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -63,6 +63,13 @@ def review_get_id(review_id=None):
         ReviewObj.delete()
         storage.save()
         return {}, 200
+    elif request.method == "PUT":
+        if review_id is None:
+            abort(404)
+        obj = storage.get(Review, review_id)
+        if not request.is_json:
+            return 'Not a JSON', 400
+
         for k, v in request.get_json().items():
             if k in ["id", "user_id", "place_id", "created_at", "update_at"]:
                 continue
